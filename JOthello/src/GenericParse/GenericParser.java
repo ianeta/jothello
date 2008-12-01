dc35946c8336e05447fc1d8b2d30f6b2dcb9f286
marcusjang <marcusjang@006b30ea-b43b-11dd-801b-97293e7877ac>
2008-12-01 00:17:00 +0000

    generic parser

--- JOthello/src/GenericParse/GenericParser.java
+++ JOthello/src/GenericParse/GenericParser.java
@@ -1,10 +1,12 @@
 package GenericParse;
 
+import backprop.Player;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import mmtothello.C;
 import mmtothello.OBoard;
 
 public class GenericParser
@@ -12,6 +14,8 @@ public class GenericParser
 
 	private String[] filenames;
 	private String path = "./generic/";
+	private int gameCount = 0;
+	private int drawGame = 0;
 
 	public GenericParser()
 	{
@@ -36,10 +40,18 @@ public class GenericParser
 			while (line != null)
 			{
 				String[] lines = line.split(" ");
-				GameMoveList game = createGameMoveList(lines[0], lines[1]);
-				line = reader.readLine();
-				
+				if (lines.length == 2)
+				{
+					GameMoveList game = createGameMoveList(lines[0], lines[1]);
+					if (game != null)
+					{
+						//place it in the machine learning bad boy
+						gameCount++;
+					}
+					line = reader.readLine();
+				}
 			}
+			System.out.println(gameCount + " " + drawGame);
 			reader.close();
 		}
 		catch (Exception ex)
@@ -51,29 +63,102 @@ public class GenericParser
 	private GameMoveList createGameMoveList(String winner, String moves)
 	{
 		List<OBoard> boards = new ArrayList<OBoard>();
-		OBoard initialBoard = new OBoard();
+		OBoard board = new OBoard();
+		char color = C.BLACK;
 
-		for (int i = 0; i < allMoves.length(); i += 2)
+		for (int i = 0; i < moves.length(); i += 2)
+		{
+			int col = char2Int(moves.charAt(i));
+			int row = Integer.parseInt(moves.substring(i + 1, i + 2)) - 1;
+			if (!canPlayerMove(board, color))
 			{
-				int col = char2Int(allMoves.charAt(i));
-				int row = Integer.parseInt(allMoves.substring(i + 1, i + 2)) - 1;
-				if (!canPlayerMove(board, color))
-				{
-					color = board.opponentOf(color);
-				}
-				if (board.canSet(row, col, color))
-				{
-					board.incMoveNumber();
-					board.set(row, col, color);
-					board.flipAll(row, col);
-					color = board.opponentOf(color);
-				}
-				else
-				{
-//					System.out.println("bad moves");
-					return;
-				}
+				color = board.opponentOf(color);
 			}
+			if (board.canSet(row, col, color))
+			{
+				board.set(row, col, color);
+				board.flipAll(row, col);
+				color = board.opponentOf(color);
+				boards.add(board);
+				board = new OBoard(board);
+			}
+			else
+			{
+				//this shouldn't happen if the input file is good
+				System.out.println("DOH!");
+				return null;
+			}
+		}
+
+		Player winningPlayer = null;
+		if (winner.equals("black"))
+		{
+			winningPlayer = Player.BLACK;
+		}
+		else if (winner.equals("white"))
+		{
+			winningPlayer = Player.WHITE;
+		}
+
+		if (winningPlayer != null)
+		{
+			//this is the situation we want
+			return new GameMoveList(boards, winningPlayer);
+		}
+
+		//if we get here, it's likely a draw game
+		drawGame++;
+		return null;
+	}
+
+	private int char2Int(char c)
+	{
+		switch (c)
+		{
+			case 'A':
+				return 0;
+			case 'a':
+				return 0;
+			case 'B':
+				return 1;
+			case 'b':
+				return 1;
+			case 'C':
+				return 2;
+			case 'c':
+				return 2;
+			case 'D':
+				return 3;
+			case 'd':
+				return 3;
+			case 'E':
+				return 4;
+			case 'e':
+				return 4;
+			case 'F':
+				return 5;
+			case 'f':
+				return 5;
+			case 'G':
+				return 6;
+			case 'g':
+				return 6;
+			case 'H':
+				return 7;
+			case 'h':
+				return 7;
+			default:
+				return -1;
+		}
+	}
+
+	private boolean canPlayerMove(OBoard board, char color)
+	{
+		if (board.canPlayerMove(color))
+		{
+			return true;
+		}
+		return false;
 	}
 
 	public static void main(String[] args)
