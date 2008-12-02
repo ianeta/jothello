e4bf1b15a509fe87424c6da4e1600c3b03aba265
panuakdet <panuakdet@006b30ea-b43b-11dd-801b-97293e7877ac>
2008-12-02 02:41:13 +0000

    Make GreedyThinker nondeterministic (random)

--- trunk/JOthello/src/mmtothello/GreedyThinker.java
+++ trunk/JOthello/src/mmtothello/GreedyThinker.java
@@ -5,6 +5,8 @@
 
 package mmtothello;
 
+import java.util.*;
+
 /**
  *
  * @author mocksuwannatat
@@ -12,6 +14,7 @@ package mmtothello;
 public class GreedyThinker implements Thinker {
   
   public static final String MY_NAME = "Mr. Greedy";
+  private Random random = null;
   
   public String getName() {
     return MY_NAME;
@@ -19,6 +22,7 @@ public class GreedyThinker implements Thinker {
   
   public RowCol nextMove(char who, OBoard board) {
     RowCol ans = new RowCol();
+    Vector<RowCol> lot = new Vector<RowCol>();
     int best = -1;
     for (int r = 0; r < board.size(); r++) {
       for (int c = 0; c < board.size(); c++) {
@@ -28,17 +32,20 @@ public class GreedyThinker implements Thinker {
             ans.row = r;
             ans.col = c;
             best = score;
+            lot.clear();
+            lot.add(ans);
+          } else if (score == best) {
+            RowCol ans2 = new RowCol();
+            ans2.row = r; 
+            ans2.col = c;
+            lot.add(ans2);
           }
         }
       }
     }
-    // pretend like we're thinking by delaying a bit
-    try {
-      Thread.sleep(3);
-    } catch (InterruptedException e) {
-      //ignore
-    }
-    return ans;
+    if (random == null) random = new Random();
+    int idx = random.nextInt(lot.size());
+    return lot.get(idx);
   }
 
 }
