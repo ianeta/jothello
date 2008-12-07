49494bcbaec26b764490b4d585ba8968aeda0d1c
panuakdet <panuakdet@006b30ea-b43b-11dd-801b-97293e7877ac>
2008-12-07 21:40:18 +0000

    initial code upload

---
+++ trunk/spikies/src/spikies/C.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spikies;
+
+/**
+ *
+ * @author mocksuwannatat
+ */
+public class C {
+
+  public static final int PANEL_WIDTH = 800;
+  public static final int PANEL_HEIGHT = 600;
+  public static final int MAX_RECORDS = 5000;
+  public static final float DEFAULT_T = 10f;
+  public static final String DEFAULT_INPUT_FILE = 
+      "/Users/mocksuwannatat/tmp/vis/ietf-sample/ipv4-first2000.csv";
+  public static final int DEFAULT_DELAY_BETWEEN_FRAMES_MS = 100;
+  public static final String NODES_INFO_FILE = 
+      "/Users/mocksuwannatat/tmp/vis/saved/nodes_info.txt";
+  public static final String SAVE_LOCATIONS = "Save Locations";
+  public static final String LOAD_LOCATIONS = "Load Locations";
+  public static final String PLAY = "Play";
+  public static final String PAUSE = "Pause";
+  public static final float BYTES_PER_KG = 10;
+  public static final float TIME_FACTOR = 100;
+  public static final float SPRING_RESTING_DISTANCE = 100;
+  public static final float SPRING_CONSTANT = 0.5f;
+  public static final float DAMPING_FACTOR = 0.99f;
+  public static final float NODES_REPEL_FACTOR = 100000;
+  public static final float SPEED_LIMIT = 100;
+}
---
+++ trunk/spikies/src/spikies/ParametersPanelListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spikies;
+
+/**
+ *
+ * @author mocksuwannatat
+ */
+public interface ParametersPanelListener {
+
+  public void valuesApplied();
+}
